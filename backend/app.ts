3b90a57e4d1e31c1f6e3faa7f4516215b348bfca
Ikhanoba Michael-Shaka <shakaikhanoba@gmail.com>
2023-08-31 20:12:54 +0100

    fix: minor updates

--- backend/app.ts
+++ backend/app.ts
@@ -20,54 +20,9 @@ import conversation from './controller/conversation';
 import message from './controller/message';
 import withdraw from './controller/withdraw';
 import chalk from 'chalk';
-
+const logEndpoints = require('custom-logger-node')
 const app = express();
 
-const logEndpoints = (req: Request, res: Response, next: NextFunction) => {
- const start = process.hrtime();
-  const { method, originalUrl, ip } = req;
-
-  res.on('finish', () => {
-    const diff = process.hrtime(start);
-    const duration = (diff[0] * 1e3 + diff[1] / 1e6).toFixed(3);
-    const statusCode = res.statusCode;
-
-    let statusColor = chalk.bgGreen;
-    if (statusCode >= 500) statusColor = chalk.bgRed;
-    else if (statusCode >= 400) statusColor = chalk.bgYellow;
-    else if (statusCode >= 300) statusColor = chalk.bgCyan;
-
-    let statusColorBg = chalk.green;
-    if (statusCode >= 500) statusColorBg = chalk.red;
-    else if (statusCode >= 400) statusColorBg = chalk.yellow;
-    else if (statusCode >= 300) statusColorBg = chalk.cyan;
-
-    let methodColor = chalk.green;
-    if (method >= 'POST') methodColor = chalk.green;
-    else if (method >= 'PATCH') methodColor = chalk.yellow;
-    else if (method >= 'GET') methodColor = chalk.cyan;
-    else if (method >= 'DELETE') methodColor = chalk.red;
-    else if (method >= 'PUT') methodColor = chalk.magenta;
-
-    const logLine = [
-      chalk.gray('[' + new Date().toISOString() + ']'),
-      methodColor(method),
-      originalUrl,
-      statusColorBg(statusCode.toString()),
-      chalk.magenta(duration + 'ms'),
-      chalk.gray('-'),
-      chalk.green(ip)
-    ].join(' ');
-
-    // Add a 20% color bar to the log line, matching the status code color
-    const colorBarLength = Math.floor(logLine.length * 0.14);
-    const colorBar = statusColor(' '.repeat(colorBarLength));
-
-    console.log(logLine + " " + colorBar);
-  });
-
-  next();
-};
 
 const options = {
   definition: {
@@ -92,10 +47,6 @@ const options = {
         url: "http://localhost:8000/api/v2",
       },
     ],
-    security: [{
-      bearerAuth: [], // name of the security scheme
-      sellerAuth: []
-  }],
   components: {
     securitySchemes: {
       bearerAuth: { // name of the security scheme
